c44867df7f6d2832986b77bf3dd4c227360094e5
MinusOneByTwelve <50650979+MinusOneByTwelve@users.noreply.github.com>
2023-02-15 16:14:56 +0530

    Delete DB/Temp/ml/experiment-tracking-with-mlflow-source directory

--- DB/Temp/ml/experiment-tracking-with-mlflow-source/bin/build.sh
+++
@@ -1,6 +0,0 @@
-source ~/.zshrc
-bdc build.yaml -o
-cp ~/tmp/curriculum/mlflow-exp-tracking-1.0.0/StudentFiles/Labs.dbc .
-databricks workspace delete /Users/$USER@databricks.com/mlflow-exp-tracking-1.0.0 -r
-databricks workspace import Labs.dbc /Users/$USER@databricks.com/mlflow-exp-tracking-1.0.0 -f DBC -l PYTHON
-rm Labs.dbc
